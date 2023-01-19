f8d814dda1125185ad19a52a3c6fb5bd75883f80
Paul Bioche <pebioche@gmail.com>
2023-01-19 17:42:34 +0100

    update readme and remove moved files

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-This is a simple app to organise and visualise the ocean impact scores from waves of change. 
+# Ocean Impact Score
+
+##This is a simple app to organise and visualise the Ocean Impact Scores from [Waves of Change](https://www.wavesofchange.earth/). 
+
+###Each public company in this list is assigned a score based on a deep learning model trained on academic papers and reliable articles about the company's environmental impact. In future versions of this tool the score updates will happen in real time. 
+
+###It was built with Next.js and Material.ui
+
+###It is not yet connected to the backend, but this is on our to do list!
+
+###Please contact the [Waves of Change Team](team@wavesofchange.earth) if you have any questions. 
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,11 @@ import Head from 'next/head'
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 
 import { SP500, SP500Sectors } from './lib/SP500_scores.js'
 import { Stoxx600, STOXX600Sectors } from './lib/STOXX600_scores.js'
 import ScoreList from './components/score-list'
-import styles from '../styles/Home.module.css'
 
 export async function getStaticProps() {
   return {
@@ -28,37 +26,33 @@ export default function Home(props: AppProps) {
   //const latestDataUploadDate = getDate(); //Need to connect with Ocean Index DB for live data each day
   const latestDataUploadDate = "November 7, 2022";
   return (
-      <span>
+      <>
       <Head>
         <title>Ocean Score App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-      <Container maxWidth="sm">
-      <Typography variant="h2" color="white">
-          Ocean Impact Score
-        </Typography>
-        <Typography variant= "body1" color= "white" sx={{fontWeight: 'bold'}}>
-          This site is a simple interface for viewing the Ocean Impact Score by Waves of Change. 
-        </Typography>
-        <Typography variant= "body1" color= "white" sx={{fontStyle: 'italic'}}> 
-          Each public company in this list is assigned a score based on a deep learning model trained on academic papers and reliable articles about the company's environmental impact. In future versions of this tool the score updates will happen in real time. 
-        </Typography>
-        <Typography variant= "body1" >{'Latest Data Dump: '+ latestDataUploadDate}</Typography>
-        <Box sx={{ width: 300, height: 50}} >
-        <Button onClick={()=>setMarketType(true)}>SP500</Button>
-        <Button onClick={()=>setMarketType(false)}>EU600</Button>
-        </Box>
-
-        <ScoreList 
-          scoresList={ marketType ? SP500 : Stoxx600 } 
-          addInfo={ marketType ? SP500Sectors :  STOXX600Sectors } 
-          {...pageProps} />
-        
+        <Container maxWidth="sm">
+          <Typography variant="h2" color="white">
+            Ocean Impact Score
+          </Typography>
+          <Typography variant= "body1" color= "white" sx={{fontWeight: 'bold'}}>
+            This site is a simple interface for viewing the Ocean Impact Score by Waves of Change. 
+          </Typography>
+          <Typography variant= "body1" color= "white" sx={{fontStyle: 'italic'}}> 
+            Each public company in this list is assigned a score based on a deep learning model trained on academic papers and reliable articles about the company's environmental impact. In future versions of this tool the score updates will happen in real time. 
+          </Typography>
+          <Typography variant= "body1" >{'Latest Data Dump: '+ latestDataUploadDate}</Typography>
+          <Box sx={{ width: 300, height: 50}} >
+            <Button onClick={()=>setMarketType(true)}>SP500</Button>
+            <Button onClick={()=>setMarketType(false)}>EU600</Button>
+          </Box>
+          <ScoreList 
+            scoresList={ marketType ? SP500 : Stoxx600 } 
+            addInfo={ marketType ? SP500Sectors :  STOXX600Sectors } 
+            {...pageProps} />
         </Container>
-        </main>
-      <footer>
-      </footer>
-      </span>
+      </main>
+      </>
   )
 }
