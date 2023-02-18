0b187c006a89a0b01d1b496d9ca4a9b30bd1aeb2
Paul Bioche <pebioche@gmail.com>
2023-02-18 23:05:09 +0100

    get live date for requests for company plots

--- pages/plot/[ticker].tsx
+++ pages/plot/[ticker].tsx
@@ -4,12 +4,16 @@ import ScorePlot from '../../components/score-plot';
 import { 
     Button, 
     ButtonGroup } from '@mui/material';
+import getDate from '../../lib/getDate';
 
 export async function getServerSideProps(context:any) {
+    const todaysDate:string = getDate() +'T00:00:00Z';
+    const lastMonthsDate:string = getlastMonthsDate() +'T00:00:00Z';
+    const lastYearsDate:string = getlastyearsDate();
     const { ticker } = context.query;
-    const url30Days = process.env.API_URL + 'time-series/esg-score/EU600?API_KEY=' + process.env.API_KEY + '&interval=1D&start=2023-01-05T10:50:35.819Z&finish=2023-02-05T10:50:35.819Z&tickers='+ticker;
-    const url1Year = process.env.API_URL + 'time-series/esg-score/EU600?API_KEY=' + process.env.API_KEY + '&interval=1M&start=2022-02-05T10:50:35.819Z&finish=2023-02-05T10:50:35.819Z&tickers='+ticker;
-    const url5Years = process.env.API_URL + 'time-series/esg-score/EU600?API_KEY=' + process.env.API_KEY + '&interval=1M&start=2016-01-05T10:50:35.819Z&finish=2023-02-05T10:50:35.819Z&tickers='+ticker;
+    const url30Days = process.env.API_URL + 'time-series/esg-score/EU600?API_KEY=' + process.env.API_KEY + '&interval=1D&start=' + lastMonthsDate + '&finish=' + todaysDate + '&tickers='+ ticker;
+    const url1Year = process.env.API_URL + 'time-series/esg-score/EU600?API_KEY=' + process.env.API_KEY + '&interval=1M&start=' + lastYearsDate + '&finish=' + todaysDate + '&tickers='+ ticker;
+    const url5Years = process.env.API_URL + 'time-series/esg-score/EU600?API_KEY=' + process.env.API_KEY + '&interval=1M&start=2016-01-05T10:50:35.819Z&finish=' + todaysDate + '&tickers='+ ticker;
     const urlArray = [ url30Days, url1Year, url5Years ];
     const data = await Promise.all(urlArray.map(async (url:string) => {
         const response = await fetch(url);
@@ -26,8 +30,12 @@ export async function getServerSideProps(context:any) {
 
 export default function Plot({data}:{data:any}) {
     const router = useRouter();
-    const { ticker } = router.query;
-
+    let { ticker } = router.query;
+    let tickerStr: string;
+    if (Array.isArray(ticker)) tickerStr = ticker[0];
+    else if (typeof ticker === 'string') tickerStr = ticker;
+    else tickerStr = '';
+    console.log(tickerStr);
     const [plotType, setPlotType] = React.useState(0);
     const plotTypeTitle: Array<String> = ["30 days", "12 months", "all time"]
 
@@ -36,7 +44,7 @@ export default function Plot({data}:{data:any}) {
             <h4>{ticker}</h4>
             <ScorePlot 
                 scores={Object.keys(data[plotType]).map((key:string) => {
-                    const tone = data[plotType][key][ticker]?.Tone
+                    const tone = data[plotType][key][tickerStr]?.Tone
                     return {t: key, y: tone};
                 })} 
                 ticker={ticker} />
