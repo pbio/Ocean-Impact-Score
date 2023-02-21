236b376b815580ad5f971c5c2768a55965f96d5e
Paul Bioche <pebioche@gmail.com>
2023-02-21 09:47:02 +0100

    added create organisedScoreData in component

--- pages/sp500.tsx
+++ pages/sp500.tsx
@@ -31,60 +31,64 @@ export async function getServerSideProps() {
     API_URL + "general-info/US500?API_KEY=" + API_KEY + tickers;
   const urls: string[] = [yearlyUrl, dailyUrl, infoUrl];
   // Get json here from Waves of Change (esg cafe) server.
-  const [yearlyScores, dailyScores, EU600Info] = await Promise.all(
+  const [yearlyScores, dailyScores, info] = await Promise.all(
     urls.map(async (url: string) => {
       const response = await fetch(url);
       const data = await response.json();
       return data;
     })
   );
-  //parse data into large array of each company
-  const organisedScoreData: any = EU600Info.map((company:any) => {
-    const ticker = company.ticker;
-    const companyScores: any = company;
-    //save day scores
-    companyScores.dailyScores = [
-      [
-        todaysDate,
-        dailyScores[todaysDate.slice(0, -5)][ticker]?.Tone,
-      ],
-      [
-        yesterdaysDate,
-        dailyScores[yesterdaysDate.slice(0, -5)][ticker]?.Tone,
-      ],
-    ];
-    //save month scores
-    const monthDateKeys = Object.keys(yearlyScores);
-    const thisMonthKey = monthDateKeys.find((date) => {
-      return date.includes(todaysDate.slice(0, 7));
-    });
-    const lastMonthKey = monthDateKeys.find((date) => {
-      return date.includes(lastMonthsDate.slice(0, 7));
-    });
-    if (thisMonthKey && lastMonthKey)
-      companyScores.monthlyScores = [
-        [thisMonthKey, yearlyScores[thisMonthKey][ticker]?.Tone],
-        [lastMonthKey, yearlyScores[lastMonthKey][ticker]?.Tone],
-      ];
-    //calc/save yearly scores
-    // const yearDateKeys = Object.keys(yearlyScores);
-    // const yearScore =
-    //   yearDateKeys.reduce((total, dateIdx) => {
-    //     return total + yearlyScores[dateIdx][company.ticker]?.Tone;
-    //   }, 0) / yearDateKeys.length;
-    // companyScores.yearlyScores = [[todaysDate, yearScore]];
-    return companyScores;
-  });
+
 
   return {
-    props: { Info: organisedScoreData },
+    props: { Info: info, yearlyScores, dailyScores },
   };
 }
 
 export default function Home({
-  Info,
+  Info, yearlyScores, dailyScores
 }: {
-  Info: any[];
+  Info: any[]; yearlyScores: any; dailyScores: any;
 }) {
-  return <ScoreList Info={Info} />;
+  const todaysDate: string = getDate() + "T00:00:00.000Z";
+  const yesterdaysDate: string = getYesterdaysDate() + "T00:00:00.000Z";
+  const lastMonthsDate: string = getlastMonthsDate() + "T00:00:00.000Z";
+    //parse data into large array of each company
+    const organisedScoreData: any = Info.map((company:any) => {
+      const ticker = company.ticker;
+      const companyScores: any = company;
+      //save day scores
+      companyScores.dailyScores = [
+        [
+          todaysDate,
+          dailyScores[todaysDate.slice(0, -5)][ticker]?.Tone,
+        ],
+        [
+          yesterdaysDate,
+          dailyScores[yesterdaysDate.slice(0, -5)][ticker]?.Tone,
+        ],
+      ];
+      //save month scores
+      const monthDateKeys = Object.keys(yearlyScores);
+      const thisMonthKey = monthDateKeys.find((date) => {
+        return date.includes(todaysDate.slice(0, 7));
+      });
+      const lastMonthKey = monthDateKeys.find((date) => {
+        return date.includes(lastMonthsDate.slice(0, 7));
+      });
+      if (thisMonthKey && lastMonthKey)
+        companyScores.monthlyScores = [
+          [thisMonthKey, yearlyScores[thisMonthKey][ticker]?.Tone],
+          [lastMonthKey, yearlyScores[lastMonthKey][ticker]?.Tone],
+        ];
+      //calc/save yearly scores
+      // const yearDateKeys = Object.keys(yearlyScores);
+      // const yearScore =
+      //   yearDateKeys.reduce((total, dateIdx) => {
+      //     return total + yearlyScores[dateIdx][company.ticker]?.Tone;
+      //   }, 0) / yearDateKeys.length;
+      // companyScores.yearlyScores = [[todaysDate, yearScore]];
+      return companyScores;
+    });
+  return <ScoreList Info={organisedScoreData} />;
 }
